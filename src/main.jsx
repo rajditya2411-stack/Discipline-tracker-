6a35dcd333476b36668941a1076a6ce0e9118128
Rajditya <rajaditya2411@gmail.com>
2026-03-06 13:52:23 +0530

    connected supabase

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,10 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+console.log(import.meta.env.VITE_SUPABASE_URL)
+
+  ReactDOM.createRoot(document.getElementById('root')).render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>,
+  )
